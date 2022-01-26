0ef1626d57938be06b36637e86262cf6189b5b01
AntoineMeunier52 <meunier.antoine52@gmail.com>
2022-01-26 21:28:34 +0100

    anim

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,57 @@
 let arr_left = document.querySelector(".arr-left");
 let arr_right = document.querySelector(".arr-right");
+let counter = 0;
 
-arr_right.addEventListener("mouseover", () => {
-	console.log("mouse on");
+let logos = document.querySelectorAll("#logo");
+let titles = document.querySelectorAll("#title");
+let descriptions = document.querySelectorAll("#description");
+let pictures = document.querySelectorAll("#picture");
+let li = document.querySelectorAll("li");
+
+arr_right.addEventListener("click", () => {
+	if (counter < 2)
+	{
+		counter++;
+		change_carrou_pos(counter);
+	}
 });
-
-let list_li = document.querySelectorAll("li");
 
 arr_left.addEventListener("click", () => {
-	console.log(list_li.length);
+	if (counter > 0)
+	{
+		counter--;
+		change_carrou_neg(counter);
+	}
 });
+
+function change_carrou_pos(counter){
+	logos[counter-1].className = "old"
+	titles[counter-1].className = "old"
+	descriptions[counter-1].className = "old"
+	pictures[counter-1].className = "old"
+	logos[counter].className = "activ"
+	titles[counter].className = "activ"
+	pictures[counter].className = "activ"
+	descriptions[counter].className = "activ"
+	li[counter].className = "activ"
+	arr_left.classList.add("activ");
+}
+
+function change_carrou_neg(counter)
+{
+	logos[counter].classList.add("activ");
+	logos[counter].classList.remove("old");
+	logos[counter + 1].classList.remove("activ");
+	titles[counter].classList.add("activ");
+	titles[counter].classList.remove("old");
+	titles[counter + 1].classList.remove("activ");
+	descriptions[counter].classList.add("activ");
+	descriptions[counter].classList.remove("old");
+	descriptions[counter + 1].classList.remove("activ");
+	pictures[counter].classList.add("activ");
+	pictures[counter].classList.remove("old");
+	pictures[counter + 1].classList.remove("activ");
+	li[counter + 1].classList.remove("activ");
+	if (counter == 0)
+		arr_left.classList.remove("activ");
+}
